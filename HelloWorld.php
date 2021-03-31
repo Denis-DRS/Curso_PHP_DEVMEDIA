375f5acece500d72e66fb037bf7ef78a04a93c56
Denis Ramos de Souza <denis-drs@hotmail.com>
2021-03-31 15:05:32 -0300

    Meu primeiro código php para fazer teste entre XAMPP e GitHub

---
+++ HelloWorld.php
@@ -0,0 +1,8 @@
+<?php
+$mensagem ="Hello World!!! <br>";
+$mensagem2 ="Este é meu primeiro script php! <br>";
+$mensagem3 = "Apenas um teste de funcionamento do Xampp+GitHub.";
+
+echo $mensagem;
+echo $mensagem2;
+echo $mensagem3;
